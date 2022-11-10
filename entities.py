cb3f254d60f2db4c705a0e7549ad9277f47c461c
TomBim <washingtonbim@hotmail.com>
2022-11-09 22:29:12 -0300

    new Classes: Bin, Commercial_Point; and finished removing class variables

--- entities.py
+++ entities.py
@@ -1,31 +1,43 @@
 from math import inf, sqrt
+from pickle import FALSE, TRUE
 import map
 import heapq
 
 #TODO: remove class variables: put them inside constructor
 class Person:
-    _id=None
-    _pos=None
-    _origin=None
-    _origin_street=None
-    _destination=None
-    _destination_street=None
-    _path=None #list of intersection points
-    _fov=None
-    _max_distance_carrying_trash=None #maximal distance that this person carries a trash
-    _time_of_consumption=None #time needed for consuming
-    _speed=None
-    _has_trash=False #if consuming something, it's false
-    _time_alive=None
-    _distance_carrying_trash=None
-    _map_=None
+    """Private variables
+        id: int
+        pos: float[2]
+        destination: Comertial_
+        path: list(Intersection)
+        fov: float
+        max_distance_carrying_trash: float
+        time_of_consumption: int (seconds)
+        speed: float (m/s)
+        has_trash: bool
+            if consuming something, it's false
+        time_alive: int (seconds)
+        distance_carrying_trash: float
 
-    def __init__(self, id, origin, destination, map_, *args):
+    """
+
+
+    def __init__(self, id: int, origin_street: map.Pos_Street, destination, map_, *args):
         self._id = id
+        self.pos = None
         self._origin = origin
         self._destination = destination
         self._map_ = map_
         self._path = self._path_planner()
+        self._fov=None
+        self._max_distance_carrying_trash=None
+        self._time_of_consumption=None
+        self._speed=None
+        self._has_trash=False    
+        self._time_alive=None
+        self._distance_carrying_trash=None
+        self._origin_street=None
+        self._destination_street=None
 
     def _path_planner(self) -> list[map.Intersection]:
         intersections_info = self._dijkstra()
@@ -97,3 +109,84 @@ class Person:
 
     def set_destination(self, destination):
         self._destination = destination
+
+class Bin:
+    """Private variables:
+        pos: float[2]
+        pos_street: [street,float]
+        capacity: float (L)
+        filling_rate: float (L/s)
+        full: bool
+        vol_trash: float (L)
+        last_time_was_empted: int (seconds)
+    """
+    def __init__(self, pos: tuple[float, float], capacity: float):
+        self._pos = pos
+        self._capacity = capacity
+        self._full = FALSE
+        self._vol_trash = 0
+        self._last_time_was_empted = -1
+        self._filling_rate = 0
+
+    def set_full(self):
+        self._full = TRUE
+
+    def set_capacity(self, new_capacity: float):
+        self._capacity = new_capacity
+    
+    def set_pos(self, new_pos: tuple[float, float]):
+        self._pos = new_pos
+
+    def put_trash(self, vol_trash):
+        self._vol_trash += vol_trash
+        if(self._vol_trash > self._capacity):
+            self._full = TRUE
+        #update filling_rate
+
+    def empty_bin(self):
+        self._vol_trash = 0
+        self._full = FALSE
+        #self._last_time_was_empted = ?
+
+class Commercial_Point:
+    """Private variables:
+        type: int
+            0: Food store
+            1: Non-food store
+            2: Job
+        customer_potential: float
+            0 -> 1
+            if just a job point, then it's worker_potential
+        trash_generation_potential: float (L)
+            the volume of the trash that a person can get in this store
+        pos: float[2]
+        pos_street: Pos_Street
+    """
+
+    def __init__(self, type: int, customer_potential: float, trash_generation_potential: float, street: map.Street, pos_in_street: float):
+        self._type = type
+        self._customer_potential = customer_potential
+        self._trash_generation_potential = trash_generation_potential
+        self._pos_street = map.Pos_Street(street, pos_in_street)
+        A = street.get_vector()[0].get_pos()
+        B = street.get_vector()[1].get_pos()
+        x = A[0] + (B[0]-A[0])*pos_in_street
+        y = A[1] + (B[1]-A[1])*pos_in_street
+        self._pos = (x,y)
+
+    def __init__(self, type: int, customer_potential: float, trash_generation_potential: float, pos_street: map.Pos_Street):
+        self._type = type
+        self._customer_potential = customer_potential
+        self._trash_generation_potential = trash_generation_potential
+        self._pos_street = pos_street
+        A = pos_street.get_street.get_vector()[0].get_pos()
+        B = pos_street.get_street.get_vector()[1].get_pos()
+        x = A[0] + (B[0]-A[0])*pos_street.get_pos_in_street
+        y = A[1] + (B[1]-A[1])*pos_street.get_pos_in_street
+        self._pos = (x,y)
+
+    def get_pos(self) -> tuple[float, float]:
+        return self._pos_street.get_street
+
+    def get_pos_in_street(self) -> float:
+        return self._pos_street.get_pos_in_street

7018cba9464519cb2ead21d3580e7813dedb30c6
Emmanuel <62708624+emmanuelsdias@users.noreply.github.com>
2022-12-02 06:48:31 -0300

    added pygame animation

---
+++ animate_lib/settings.py
@@ -0,0 +1,13 @@
+FPS = 60
+WIDTH = 1080
+HEIGHT = 840
+
+BLACK       = (0, 0, 0)
+WHITE       = (255, 255, 255)
+LIGHT_GRAY  = (200, 200, 200)   
+DARK_GRAY   = (150, 150, 150)
+RED         = (255, 0, 0)
+GREEN       = (0, 255, 0)
+BLUE        = (0, 0, 255)
+
+DEFAULT_FONT = "assets/fonts/---.ttf"

2a46f4c28b4f302ed204d172336aade9f9d4d8dc
TomBim <washingtonbim@hotmail.com>
2022-12-07 00:43:32 -0300

    fixed walking

--- potentials.py
+++ potentials.py
@@ -32,70 +32,56 @@ class Potential:
         
 
 class Pontual_Charge:
-    def __init__(self, q: float, pos_street: map.Pos_Street, mapa: map.Map, id: int) -> None:
+    def __init__(self, q: float, pos_street: map.Pos_Street, mapa: map.Map, id: int, pot_matrix: np.array, K: float) -> None:
         self._q = q
         self._pos_street = pos_street
-        self._need_update_distances = True
-        self._need_update_potentials = True
         self._map_ = mapa
         self._id = id
+        self._pot_matrix = pot_matrix
+        self._K = K
+        self._need_update_distances = True
+        self._intersections_distance = []
         self.update()
 
     def set_q(self, new_q: float):
         self._q = new_q
         self._need_update_potentials = True
 
-    def set_need_update(self, distances: bool, potentials: bool):
-        """You can't set the neediness as false in this function. If you want to, use set_updated
+    # def set_need_update(self, distances: bool, potentials: bool):#TODO: its wrong
+    #     """You can't set the neediness as false in this function. If you want to, use set_updated
 
-        Args:
-            distances (bool): set as needing an update on distances?
-            potentials (bool): set as needing an update on potentials?
-        """
-        if distances:
-            self._need_update_distances = True
-            self._need_update_potentials = True
-        elif potentials:
-            self._need_update_potentials = True
+    #     Args:
+    #         distances (bool): set as needing an update on distances?
+    #         potentials (bool): set as needing an update on potentials?
+    #     """
+    #     if distances:
+    #         self._need_update_distances = True
+    #         self._need_update_potentials = True
+    #     elif potentials:
+    #         self._need_update_potentials = True
 
-    def set_updated(self, distances: bool, potentials: bool):
-        """You can't set the neediness as true in this function. If you want to, use set_need_update
+    # def set_updated(self, distances: bool, potentials: bool):#TODO: its wrong
+    #     """You can't set the neediness as true in this function. If you want to, use set_need_update
 
-        Args:
-            distances (bool): set as distances updated?
-            potentials (bool): set as potentials updated?
-        """
-        if distances:
-            self._need_update_distances = False
-        if potentials:
-            self._need_update_potentials = (False or self._need_update_distances)
+    #     Args:
+    #         distances (bool): set as distances updated?
+    #         potentials (bool): set as potentials updated?
+    #     """
+    #     if distances:
+    #         self._need_update_distances = False
+    #     if potentials:
+    #         self._need_update_potentials = (False or self._need_update_distances)
 
     def update(self):
-        if self._need_update_distances:
-            self._intersections_distance = fcs.dijkstra(self._map_, self._pos_street)['distance to origin']
-            self._need_update_distances = False
-            for intersection_dist in self._intersections_distance:
-                self._intersections_potentials = Ke * self._q / intersection_dist
-            self._need_update_potentials = False
-        elif self._need_update_potentials:
-            for intersection_dist in self._intersections_distance:
-                self._intersections_potentials = Ke * self._q / intersection_dist
-            self._need_update_potentials = False
-
-    def update_pot_distr(self, matrix: np.array):
-        """update the matrix if it's necessary
-
-        Args:
-            matrix (np.array): matrix with the potential distribution
-        """
-        if self._need_update_distances or self._need_update_potentials
-
-    def calculate_potential(self, pos_street)
-
-class Gravity_Point
-
-class Gravity_Potential
-
-class Electrical_Point:
-
-class Electrical_Potential:
+        if len(self._intersections_distance) == 0:
+            if self._need_update_distances:
+                self._intersections_distance = np.array(fcs.dijkstra(self._map_, self._pos_street)['distance to origin'])
+                self._need_update_distances = False
+                self._intersections_potentials = np.array([])
+                for intersection_dist in self._intersections_distance:
+                    self._intersections_potentials = np.append(self._intersections_potentials(self._K * self._q / intersection_dist
+                self._need_update_potentials = False
+            elif self._need_update_potentials:
+                for intersection_dist in self._intersections_distance:
+                    self._intersections_potentials = self._K * self._q / intersection_dist
+                self._need_update_potentials = False

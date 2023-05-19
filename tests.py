2a1209c7f3c845ba192c36d2506358c6efc3abbc
TomBim <washingtonbim@hotmail.com>
2023-05-19 00:01:34 -0300

    one more function to tests

--- tests.py
+++ tests.py
@@ -1,3 +1,5 @@
+import random as rand
+
 from bin_lib import *
 
 file_intersections = "bin_lib/intersection_points.txt"
@@ -10,3 +12,8 @@ def map_test(files: list[str]):
     for x in streets:
         x.get_length()
     some_functions.plot_map(intersections, streets)
+
+def gen_rand_pos_street(mapa: map.Map) -> map.Pos_Street:
+    n_streets = len(mapa.get_streets_list())
+    s = mapa.get_street(rand.randrange(n_streets))
+    return map.Pos_Street(s, rand.random())    
